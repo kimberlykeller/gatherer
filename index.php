f5a7ea1a9dfad7ddcbaa615093d0c6ce32565b4a
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-14 15:40:09 -0600

    added persona content

--- index.php
+++ index.php
@@ -7,19 +7,22 @@
 	<body>
 		<h1>Database Design Project for Gatherer</h1>
 		<h3>Persona</h3>
-		Aleck is 25 years old with a degree in Electrical Engineering. He enjoys tabletop gaming and problem solving.
+		<p>Aleck is 25 years old with a degree in Electrical Engineering. He enjoys tabletop gaming and problem solving.
 		In his spare time he builds or fixes small things by hand in his garage workshop, inlcudes things like woodworking
 		or bike repair. He is detail oriented, focused, and introverted, but enjoys small doses of social situations, such as Magic: The Gathering
 		tournaments or playing with his friends casually. He has a handful of interests that he dives deeply into, such as sci-fi and fantasy,
 		craft beer, etc.
-
+		</p>
+		<p>
 		He is technology oriented and will have some combination of newer mobile, tablet, laptop or desktop access. He may or may not
 		have put together his own desktop tower, but has the skills to do so.
-
+		</p>
+		<p>
 		Other gaming interests include: Settlers of Catan, Sentinels of the Multiverse, Chez Geek, Call of Cthulhu, Arkham Horror.
-
-		He desires to gian
-
+		</p>
+		<p>
+		He desires to g
+		</p>
 
 		<p>Details of persona</p>
 		<h3>Use Cases</h3>
