49275bf955772ceb84e2153dfbf6780fa79e6420
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-14 15:15:03 -0600

    CONT. added structure for content, header tags, paragraphs, unordered lists

--- index.php
+++ index.php
@@ -17,5 +17,17 @@
 			<li>2</li>
 			<li>3</li>
 		</ul>
+		<p><strong>Attributes</strong></p>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+		<p><strong>Keys</strong></p>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
 	</body>
 </html>
