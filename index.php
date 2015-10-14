6c2f02e8eda72cb91ad329b4d62b7d4428dc5b2c
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-14 15:11:45 -0600

    added structure for content, header tags, paragraphs, unordered lists

--- index.php
+++ index.php
@@ -5,6 +5,17 @@
 		<title>Database Design for Gatherer</title>
 	</head>
 	<body>
-
+		<h1>Database Design Project for Gatherer</h1>
+		<h3>Persona</h3>
+		<p>Details of persona</p>
+		<h3>Use Cases</h3>
+		<p>Details of use cases</p>
+		<h3>Data Breakdown</h3>
+		<p><strong>Data Entities</strong></p>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
 	</body>
 </html>
