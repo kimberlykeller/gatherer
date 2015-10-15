6cc5db7b59d2adc9fd9e165d186a7960b9e19cb0
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-15 07:45:43 -0600

    finalized list and added some notes about categories and sub categories

--- index.php
+++ index.php
@@ -22,21 +22,71 @@
 		He desires to gain accurate information about the Magic: The Gathering card or groupings of cards that he has searched for.
 		</p>
 		<h3>Use Cases</h3>
+		<p><strong>Use Case 1 &rarr; Aleck Wants A Specific Card</strong></p>
 		<ol>
-			<li>	Aleck goes to gatherer.wizards.com with a specific card in mind.He wants to know all of the essential information
+			<li>	Aleck goes to gatherer.wizards.com with a specific card in mind. He wants to know all of the essential information
 		about this card.
 			</li>
 			<li>	Gatherer has a basic search funciton that he can type or copy/paste the card name into.</li>
 			<li>	Aleck begins typing the card name.</li>
 			<li>	Gatherer anticipates Aleck's request based on the first few characters and makes suggestions.</li>
 			<li>	Aleck can either type the full card name or select a suggestion.</li>
-			<li>	Aleck hits enter or clicks the "search" button.</li>
-			<li>	Gatherer fetches the page with the information requested.</li>
+			<li>	Aleck hits enter or clicks the &quot;search&quot; button.</li>
+			<li>	Gatherer fetches the page with the specific card information requested.</li>
 		.</ol>
+		<p><strong>Use Case 2 &rarr; Aleck Wants A Group of Cards </strong></p>
+		<ol>
+			<li>	Aleck goes to gatherer.wizards.com with a type of card in mind. He wants to see all the cards with this type.
+			(for example, he wants to see all cards with type &quot;creature&quot;)</li>
+			<li>	Gatherer allows him to check a box to indicate he wants to search by type, not by name</li>
+			<li>	Aleck enters the type into the search box and hits enter or hits the &quot;search&quot; button</li>
+			<li>	Gatherer fetches all the cards with this type and displays them in a list with a brief summary of each card</li>
+			<li>	Aleck clicks on the name or picture of the card he is interested in</li>
+			<li>	Gatherer fetches the specific information on that card and loads a page to display it</li>
+		</ol>
 
 		<hr>
 		<h3>Data Breakdown</h3>
 		<p><strong>Data Entities</strong></p>
+		<p>User</p>
+		<ul>
+			<li>Username</li>
+			<li>Email Address</li>
+			<li>Comments</li>
+			<li>DCI Number</li>
+			<li>Name</li>
+			<li><strong>Primary Key</strong> Email address</li>
+		</ul>
+		<p>Card</p>
+		<ul>
+			<li>Card Picture</li>
+			<li>Card Name</li>
+			<li>Mana Cost</li>
+			<li>Converted Mana Cost</li>
+			<li>Color Identity</li>
+			<li>Rules Text</li>
+			<li>Flavor Text</li>
+			<li>Types</li>
+			<li>P/T &#40;power/toughness&#41;</li>
+			<li>Rarity</li>
+			<li>Artists</li>
+			<li>Rulings?</li>
+			<li>Sets and Expansions</li>
+			<li>Mark</li>
+			<li>Community Rating</li>
+			<li><strong>Primary Key</strong> Multiverse ID</li>
+		</ul>
+		<p>The Game</p>
+			<ul>
+				<li>Format</li>
+				<li>Rulings?</li>
+			</ul>
+		<p>Some Notes:</p>
+		<ul>
+			<li>Types &gt; Subtypes</li>
+			<li>Mana Cost &gt; CMC</li>
+			<li>Blocks &gt; Sets </li>
+		</ul>
 
 	</body>
 </html>
