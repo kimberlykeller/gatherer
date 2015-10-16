04d29f92a7b189bf51e88ff0ac55654ac0b8ea12
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-16 11:52:45 -0600

    started adding tables in setup file

--- setup.php
+++ setup.php
@@ -1,7 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta charset="UTF-8">
+		<meta charset="UTF-8">
+		<title>Setting Up the Databases</title>
 	</head>
+	<body>
+		<h3>Setting up....</h3>
 
+		CREATE TABLE card (
+			cardPicture
+			cardName
+			convManaCost
+			type
+			rulesText
+			rarity
+			cardNumber
+
+		<br>...Done
+
+	</body>
 </html>
