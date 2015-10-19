9fafa96d26f4c048ab06afede0384024ade3923c
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-19 08:22:08 -0600

    added sql statements to gatherer page in a code tag

--- gatherer-database-tables.sql
+++ gatherer-database-tables.sql
@@ -1,25 +1,28 @@
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS expansion;
+
 CREATE TABLE expansion (
-			expanName VARCHAR(128) NOT NULL,
-			expanReleaseDate DATE NOT NULL,
-			expanNumberOfCards SMALLINT UNSIGNED NOT NULL,
-			expanOrSet TINYINT UNSIGNED NOT NULL,
-			expanId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-			INDEX(releaseDate),
-			UNIQUE(expanName),
-			PRIMARY KEY(expanId),
-		);
+	expanName VARCHAR(128) NOT NULL,
+	expanReleaseDate DATE NOT NULL,
+	expanNumberOfCards SMALLINT UNSIGNED NOT NULL,
+	expanOrSet TINYINT UNSIGNED NOT NULL,
+	expanId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	INDEX(expanReleaseDate),
+	UNIQUE(expanName),
+	PRIMARY KEY(expanId)
+);
 
 
-		CREATE TABLE card (
-			cardName VARCHAR(128) NOT NULL,
-			convManaCost TINYINT NOT NULL,
-			type VARCHAR (64),
-			rulesText VARCHAR(1000),
-			rarity VARCHAR(20),
-			multiverseId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-			expanId INT UNSIGNED NOT NULL,
-			INDEX(expanId),
-			FOREIGN KEY (expanId) REFERENCES expansion(expanId),
-			UNIQUE(cardName),
-			PRIMARY KEY(multiverseId),
-		);
+CREATE TABLE card (
+	cardName VARCHAR(128) NOT NULL,
+	convManaCost TINYINT NOT NULL,
+	type VARCHAR (64),
+	rulesText VARCHAR(1000),
+	rarity VARCHAR(32),
+	multiverseId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	expanId INT UNSIGNED NOT NULL,
+	INDEX(expanId),
+	FOREIGN KEY (expanId) REFERENCES expansion(expanId),
+	UNIQUE(cardName),
+	PRIMARY KEY(multiverseId)
+);
--- index.php
+++ index.php
@@ -76,6 +76,36 @@
 		<h3>Conceptual Model</h3>
 		<p>Many to many </p>
 		<img src="img/gatherer-erd.svg" alt="the ERD for the gatherer project">
+		<h3>Code Sample</h3>
+		<code>DROP TABLE IF EXISTS card;
+			DROP TABLE IF EXISTS expansion;
+
+			CREATE TABLE expansion (
+			expanName VARCHAR(128) NOT NULL,
+			expanReleaseDate DATE NOT NULL,
+			expanNumberOfCards SMALLINT UNSIGNED NOT NULL,
+			expanOrSet TINYINT UNSIGNED NOT NULL,
+			expanId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+			INDEX(expanReleaseDate),
+			UNIQUE(expanName),
+			PRIMARY KEY(expanId)
+			);
+
+
+			CREATE TABLE card (
+			cardName VARCHAR(128) NOT NULL,
+			convManaCost TINYINT NOT NULL,
+			type VARCHAR (64),
+			rulesText VARCHAR(1000),
+			rarity VARCHAR(32),
+			multiverseId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+			expanId INT UNSIGNED NOT NULL,
+			INDEX(expanId),
+			FOREIGN KEY (expanId) REFERENCES expansion(expanId),
+			UNIQUE(cardName),
+			PRIMARY KEY(multiverseId)
+			);
+		</code>
 
 
 
