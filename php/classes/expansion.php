f0713ff204c2f31941037403d6a8359e1e94ad5a
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-21 14:07:59 -0600

    added accessor method to class Expansion for primary key, expanId

--- php/classes/expansion.php
+++ php/classes/expansion.php
@@ -14,27 +14,44 @@ class Expansion {
 	/**
 	 * id for this expansion; this is the primary key
 	 * @var int $expanId
-	 */
+	 **/
 	private $expanId;
 	/**
 	 * name of the expansion
 	 * @var string $expanName
-	 */
+	 **/
 	private $expanName;
 	/**
 	 * number of cards printed in the expansion
 	 * @var int $expanNumberOfCards
-	 */
+	 **/
 	private $expanNumberOfCards;
 	/**
 	 * a 1 or a 0 indicating if the expansion is also a set. 1 for if set is true
 	 * @var int $expanOrSet
-	 */
+	 **/
 	private $expanOrSet;
 	/**
 	 * the date that the expansion was released on
 	 * @var date $expanReleaseDate
-	 */
+	 **/
 	private $expanReleaseDate;
+
+	/**
+	 * accessor method for expan id
+	 *
+	 * @return mixed value of expan id
+	 **/
+	public function getExpanId() {
+		return($this->expanId);
+	}
+
+	/**
+	 * mutator method for expanId
+	 *
+	 * @param mixed $newExpanId new value of tweet id
+	 * @throws InvalidArugmentException if $newExpanId is not an integer
+	 * @throws RangeException if $newTweetId is not positive
+	 **/
 }
 
